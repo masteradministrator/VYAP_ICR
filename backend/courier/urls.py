3d6917c865cc2b4e96126127b4b9b36c0b6e785b
mukund201501@gmail.com <gandlurm@gmail.com>
2016-02-27 21:23:28 +0530

    basic

---
+++ backend/courier/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from rest_framework import routers
+from rest_framework import generics, permissions, status, response, views 
+from icr.views import InwardCourierRegistryViewSet, InwardCourierRegistryView
+from my_auth.views import RegisterViewSet, LoginView
+
+router = routers.DefaultRouter(trailing_slash = False)
+router.register(r'icr', InwardCourierRegistryViewSet)
+router.register(r'register', RegisterViewSet)
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'icr.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^vyaaptiadmin/', include(admin.site.urls)),
+    url(r'^', include(router.urls), name='home'),
+    #url(r'icr', InwardCourierRegistryView.as_view(), name='icr'),
+    url(r'login', LoginView.as_view(), name='login'))
---
+++ frontend/templates/js/dashboard.js
@@ -0,0 +1,36 @@
+ $("#icr-button").click(function(event){
+		 event.preventDefault();
+
+	 var serialized = $("form").serializeArray();
+	 var s = '';
+        var data = {};
+        for(s in serialized){
+            data[serialized[s]['name']] = serialized[s]['value']
+        }
+        formData = JSON.stringify(data);
+	 console.log(formData);	 
+	 $('form').fadeOut(500);
+	 $('.wrapper').addClass('form-success');
+
+
+	 var url = "http://localhost:8000/icr";
+     var xhr = new XMLHttpRequest();
+     xhr.open("POST", url, true);
+
+     //Send the proper header information along with the request
+     xhr.setRequestHeader("Content-type", "application/json");
+
+     xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState == 4 && xhr.status == 200) {
+        	var data = xhr.responseText;
+        	console.log("response : " + data);
+        	data = JSON.parse(data); 
+            sessionStorage.token = data.auth_token;
+            window.location="dashboard.html";
+        }
+     }
+     
+     xhr.send(formData);
+     //   var response = xhr.send(formData);
+        
+ });
