e6418f5a1436dda409e637b548f8a779be5404ce
mukund201501@gmail.com <gandlurm@gmail.com>
2016-02-28 17:13:04 +0530

    message

--- backend/courier/urls.py
+++ backend/courier/urls.py
@@ -6,13 +6,13 @@ from icr.views import InwardCourierRegistryViewSet, InwardCourierRegistryView
 from my_auth.views import RegisterViewSet, LoginView
 
 router = routers.DefaultRouter(trailing_slash = False)
-router.register(r'icr', InwardCourierRegistryViewSet)
-router.register(r'register', RegisterViewSet)
+router.register(r'/icr', InwardCourierRegistryViewSet)
+router.register(r'/register', RegisterViewSet)
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'icr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^vyaaptiadmin/', include(admin.site.urls)),
+    url(r'^/vyaaptiadmin/', include(admin.site.urls)),
     url(r'^', include(router.urls), name='home'),
     #url(r'icr', InwardCourierRegistryView.as_view(), name='icr'),
-    url(r'login', LoginView.as_view(), name='login'))
+    url(r'/login', LoginView.as_view(), name='login'))
