4340c2a648886fffdff61051336bfbb1a6cd4dfd
mukund201501@gmail.com <gandlurm@gmail.com>
2016-02-28 22:14:31 +0530

    message

--- backend/courier/urls.py
+++ backend/courier/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from rest_framework import generics, permissions, status, response, views 
 from icr.views import InwardCourierRegistryViewSet, InwardCourierRegistryView
 from my_auth.views import RegisterViewSet, LoginView
+from courier import settings
 
 router = routers.DefaultRouter(trailing_slash = False)
 router.register(r'icr', InwardCourierRegistryViewSet)
@@ -14,5 +15,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'vyaaptiadmin/', include(admin.site.urls)),
     url(r'', include(router.urls), name='home'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
     #url(r'icr', InwardCourierRegistryView.as_view(), name='icr'),
     url(r'login', LoginView.as_view(), name='login'))
