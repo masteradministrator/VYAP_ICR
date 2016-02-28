127fe8fbf260c8ae1540ef9324a2e45e3d2781e6
mukund201501@gmail.com <gandlurm@gmail.com>
2016-02-28 19:26:47 +0530

    message

--- backend/courier/settings.py
+++ backend/courier/settings.py
@@ -148,6 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+print(STATIC_ROOT)
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
